e9af5428e5f410481ae87cf619cc28080dfee9f5
Carsten Schnober <schnober@ukp.informatik.tu-darmstadt.de>
2015-11-09 14:25:39 +0100

    Implement unit tests #3
     - lda-asl/LdaEstimationPipelineTest.java: license header added.

--- lda-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/examples/lda/LdaEstimationPipelineTest.java
+++ lda-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/examples/lda/LdaEstimationPipelineTest.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2015
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.examples.lda;
 
 import cc.mallet.topics.ParallelTopicModel;
@@ -8,9 +25,6 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by schnober on 09.11.15.
- */
 public class LdaEstimationPipelineTest
 {
 
