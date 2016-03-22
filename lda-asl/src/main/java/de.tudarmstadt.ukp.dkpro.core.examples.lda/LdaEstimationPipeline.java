0b7505a41576c0f4667c640cc99395eadf5709ca
Carsten Schnober <schnober@ukp.informatik.tu-darmstadt.de>
2016-03-22 15:05:38 +0100

    No issue. Removing unnecessary CasDumpWriter and dependency.

--- lda-asl/src/main/java/de.tudarmstadt.ukp.dkpro.core.examples.lda/LdaEstimationPipeline.java
+++ lda-asl/src/main/java/de.tudarmstadt.ukp.dkpro.core.examples.lda/LdaEstimationPipeline.java
@@ -21,7 +21,6 @@ import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
 import de.tudarmstadt.ukp.dkpro.core.mallet.topicmodel.MalletTopicModelEstimator;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
 import de.tudarmstadt.ukp.dkpro.core.stopwordremover.StopWordRemover;
-import de.tudarmstadt.ukp.dkpro.core.testing.dumper.CasDumpWriter;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
@@ -77,9 +76,6 @@ public class LdaEstimationPipeline
         AnalysisEngineDescription lda = createEngineDescription(MalletTopicModelEstimator.class,
                 MalletTopicModelEstimator.PARAM_TARGET_LOCATION, TARGET_FILE,
                 MalletTopicModelEstimator.PARAM_N_ITERATIONS, ITERATIONS);
-        AnalysisEngineDescription writer = createEngineDescription(CasDumpWriter.class,
-                CasDumpWriter.PARAM_TARGET_LOCATION, OUTPUT_FILE);
-
-        SimplePipeline.runPipeline(reader, segmenter, stopwordRemover, lda, writer);
+        SimplePipeline.runPipeline(reader, segmenter, stopwordRemover, lda);
     }
 }
