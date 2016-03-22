233a82e10c37a411f7b48b49ba6710e35bd2ef52
Carsten Schnober <schnober@ukp.informatik.tu-darmstadt.de>
2016-03-22 14:58:29 +0100

    Example pipeline for computing word embeddings #10
    - pom.xml: module wordembeddings-asl added
    - module wordembeddings-asl added

---
+++ wordembeddings-asl/src/main/java/LemmaEmbeddingsPipeline.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright 2016
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.mallet.wordembeddings.WordEmbeddingsEstimator;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordLemmatizer;
+import de.tudarmstadt.ukp.dkpro.core.stopwordremover.StopWordRemover;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+
+public class LemmaEmbeddingsPipeline
+{
+    private static final File TARGET_DIR = new File("target/");
+    private static final String LANGUAGE = "en";
+    private static final File STOPWORD_FILE = new File("src/main/resources/stopwords_en.txt");
+    private static final String DEFAULT_SOURCE_DIR = "src/main/resources/texts/*";
+    private static final int NUM_THREADS = 1;   // do not use multiple threads for very small (test) datasets or the estimator may run infinitely!
+    private static final String TYPENAME = Token.class.getTypeName() + "/lemma/value";
+
+    public static void main(String[] args)
+            throws IOException, UIMAException
+    {
+        String inputDir = args.length > 0 ? args[0] : DEFAULT_SOURCE_DIR;
+
+        CollectionReaderDescription reader = createReaderDescription(TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, inputDir,
+                TextReader.PARAM_LANGUAGE, LANGUAGE);
+        AnalysisEngineDescription segmenter = createEngineDescription(OpenNlpSegmenter.class);
+        AnalysisEngineDescription stopwordRemover = createEngineDescription(StopWordRemover.class,
+                StopWordRemover.PARAM_MODEL_LOCATION, STOPWORD_FILE);
+        AnalysisEngineDescription lemmatizer = createEngineDescription(StanfordLemmatizer.class);
+        AnalysisEngineDescription embeddings = createEngineDescription(
+                WordEmbeddingsEstimator.class,
+                WordEmbeddingsEstimator.PARAM_TARGET_LOCATION, TARGET_DIR,
+                WordEmbeddingsEstimator.PARAM_NUM_THREADS, NUM_THREADS,
+                WordEmbeddingsEstimator.PARAM_TOKEN_FEATURE_PATH, TYPENAME);
+
+        SimplePipeline.runPipeline(reader, segmenter, lemmatizer, stopwordRemover, embeddings);
+    }
+}
